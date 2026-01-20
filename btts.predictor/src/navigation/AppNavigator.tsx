627181cb2fb92377ecc3e40349c4bd7cfe4d3b6d
darkotosic <darkotosic1986@gmail.com>
2026-01-20 14:38:27 +0100

    Add drawer navigation to btts predictor

--- btts.predictor/src/navigation/AppNavigator.tsx
+++ btts.predictor/src/navigation/AppNavigator.tsx
@@ -1,23 +1,10 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import TodayScreen from '../screens/TodayScreen';
-import TomorrowScreen from '../screens/TomorrowScreen';
-import Top3Screen from '../screens/Top3Screen';
-import TicketsScreen from '../screens/TicketsScreen';
-import SettingsScreen from '../screens/SettingsScreen';
-
-const Tab = createBottomTabNavigator();
+import DrawerNavigator, { navigationTheme } from './DrawerNavigator';
 
 export default function AppNavigator() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="Today" component={TodayScreen} />
-        <Tab.Screen name="Tomorrow" component={TomorrowScreen} />
-        <Tab.Screen name="Top3" component={Top3Screen} />
-        <Tab.Screen name="Tickets" component={TicketsScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
+    <NavigationContainer theme={navigationTheme}>
+      <DrawerNavigator />
     </NavigationContainer>
   );
 }
